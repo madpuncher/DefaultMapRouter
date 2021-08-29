af4568335c571720a87455ecb21b73e73f7229b5
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2021-08-29 12:51:53 +0300

    Initial Commit

---
+++ MapRouter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapRouter
+//
+//  Created by Eʟᴅᴀʀ Tᴇɴɢɪᴢᴏᴠ on 29.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
